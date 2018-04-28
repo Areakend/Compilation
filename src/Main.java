b6844752c2e860a47b8b53bf4b8afbd183a9d87d
Raphael Ohanian <raphael.ohanian@telecomnancy.eu>
2018-04-28 23:50:21 +0200

    Push fonctionne pas

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ import static Expr.TreeParser.analyseRec;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String file = "ExemplesRust/ex2.rs";
+        String file = "ExemplesRust/ex1.rs";
 
         System.out.println("File to load: " + file);
 
